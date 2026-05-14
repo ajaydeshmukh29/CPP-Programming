2009b41b2ca3acee8bf12f5d00f62dc9e7c2ce44
ajaydeshmukh29 <deshmukhajay950@gmail.com>
2026-05-14 05:50:26 +0530

    Reference

---
+++ ReferenceDemoX.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ // we can create one reference to one variable
+
+ int no = 11;
+ int &x = no;
+
+ // we can create multiple reference for signle variable
+ int &y = no;
+
+ // we can create reference to reference
+ int &z = y;
+
+ // we can create reference to pointer
+ int *p = &no;
+ int *(&q) = p;
+
+    return 0;
+}
